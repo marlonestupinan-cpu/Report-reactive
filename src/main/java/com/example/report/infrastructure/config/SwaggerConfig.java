abef9c3a4459bbc3a577da6fdc217de93ef9bbd0
Marlon <marlone2@outlook.com>
2025-10-16 17:11:47 -0500

    Adding openAPI documentation

---
+++ src/main/java/com/example/report/infrastructure/config/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package com.example.report.infrastructure.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Report API")
+                        .version("1.0")
+                        .description("Reactive API documentation for report microservice"));
+    }
+}
